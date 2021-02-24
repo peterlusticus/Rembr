b98e90dd6494fd4845415f2c3e7497309f42e093
peterlusticus <71382635+peterlusticus@users.noreply.github.com>
2021-02-24 10:29:58 +0100

    Update colorchange.py

--- cnc/colorchange.py
+++ cnc/colorchange.py
@@ -6,6 +6,7 @@ import time
 filename = 'test.txt'
 dir_left = GPIO.HIGH
 dir_right = GPIO.LOW
+color_list = ["schwarz","weiß","orange","lila","beige","blau","gelb","grün"]
 
 #Define and set up Pins
 GPIO.setmode(GPIO.BOARD)
@@ -61,106 +62,113 @@ GPIO.setup(m6, GPIO.OUT)
 GPIO.setup(s9, GPIO.IN)
 GPIO.setup(s10, GPIO.IN)        
 
-
+#----------------------------------HELP-METHODS----------------------------------
 #engage needle gear
 def needleGear_engage():
-     if GPIO.input(s2, GPIO.HIGH):
-            GPIO.output(m2, GPIO.HIGH)
+    if GPIO.input(s2, GPIO.HIGH):
+        GPIO.output(m2, GPIO.HIGH)
     
     if GPIO.input(s1, GPIO.HIGH):
-            GPIO.output(m2, GPIO.LOW)
-
+        GPIO.output(m2, GPIO.LOW)
 
 #disengage needle gear 
 def NeedleGear_disengage():
     if GPIO.input(s1, GPIO.HIGH):
          GPIO.output(m2, GPIO.HIGH)
- 
     if GPIO.input(s2, GPIO.HIGH):
-            GPIO.output(m2, GPIO.LOW)
-
-    else print"needle gear Motor at a wrong position"
+        GPIO.output(m2, GPIO.LOW)
+    else:
+        print("needle gear Motor at a wrong position")
 
 
 #move Needle Storage Arm up
 def NeedleStorageArm_up():
-          if GPIO.input(s5, GPIO.HIGH):
-                GPIO.output(m4, GPIO.HIGH)
-          if GPIO.input(s6, GPIO.HIGH):
-            GPIO.output(m4, GPIO.LOW)
+    if GPIO.input(s5, GPIO.HIGH):
+        GPIO.output(m4, GPIO.HIGH)
+    if GPIO.input(s6, GPIO.HIGH):
+    GPIO.output(m4, GPIO.LOW)
 
 
 #move Needle Storage Arm down
 def NeedleStorageArm_down():
-          if GPIO.input(s6, GPIO.HIGH):
-                GPIO.output(m4, GPIO.HIGH)
-          if GPIO.input(s5, GPIO.HIGH):
-            GPIO.output(m4, GPIO.LOW)
+    if GPIO.input(s6, GPIO.HIGH):
+        GPIO.output(m4, GPIO.HIGH)
+    if GPIO.input(s5, GPIO.HIGH):
+        GPIO.output(m4, GPIO.LOW)
 
 
 #cut Rope under needle
 def cutRope():
     if GPIO.input(s9, GPIO.HIGH):
-            GPIO.output(m6, GPIO.HIGH)
+        GPIO.output(m6, GPIO.HIGH)
     if GPIO.input(s10, GPIO.HIGH):
-            GPIO.output(m6, GPIO.LOW)
-
-    else print"rope Motor is at a wrong position"
+        GPIO.output(m6, GPIO.LOW)
+    else:
+        print("rope Motor is at a wrong position")
 
 
 #free Rope after cuting  
 def free_Rope():  
-        if GPIO.input(s3, GPIO.HIGH):
-                GPIO.output(m3, GPIO.HIGH)
-        if GPIO.input(s4, GPIO.HIGH):
-                GPIO.output(m3, GPIO.LOW)
+    if GPIO.input(s3, GPIO.HIGH):
+        GPIO.output(m3, GPIO.HIGH)
+    if GPIO.input(s4, GPIO.HIGH):
+        GPIO.output(m3, GPIO.LOW)
         
-        if GPIO.input(s4, GPIO.HIGH):
-                 GPIO.output(m3, GPIO.HIGH)
-        if GPIO.input(s3, GPIO.HIGH):
-                 GPIO.output(m3, GPIO.LOW)
+    if GPIO.input(s4, GPIO.HIGH):
+        GPIO.output(m3, GPIO.HIGH)
+    if GPIO.input(s3, GPIO.HIGH):
+         GPIO.output(m3, GPIO.LOW)
 
 
 #Move head right
 def MoveHead_Right():
-        if GPIO.input(s7, GPIO.HIGH), 
-            GPIO.output(m5, GPIO.HIGH)
-
-        if GPIO.input(s8, GPIO.HIGH):
-            GPIO.output(m5, GPIO.LOW)
+    if GPIO.input(s7, GPIO.HIGH), 
+        GPIO.output(m5, GPIO.HIGH)
+    if GPIO.input(s8, GPIO.HIGH):
+        GPIO.output(m5, GPIO.LOW)
 
 #Move Head left
 def MoveHead_Left():
     if GPIO.input(s7, GPIO.HIGH), 
-            GPIO.output(m5, GPIO.HIGH)
-
+        GPIO.output(m5, GPIO.HIGH)
         if GPIO.input(s8, GPIO.HIGH):
             GPIO.output(m5, GPIO.LOW)
 
-color_list = [0,schwarz,weiß,orange,lila,beige,blau,gelb,grün]
 
+#----------------------------------MAIN-METHODS----------------------------------
+#Change color
 def colorChange(color)
-    msc #digital code for high resistance = low motor speed
+    msc #TODO digital code for high resistance = low motor speed
     two low speed rotations
-    nps #the needle must be sensed down
+    nps #TODO the needle must be sensed down
     cutRope()
-    msc #low Speed to up position
+    msc #TODO low Speed to up position
     GPIO.output(m1, GPIO.LOW)
     free_Rope()
     NeedleGear_disengage()
     NeedleStorageArm_up()
 
-    for color in color_list:
-        C = color_list.index(color)
-        color_befor_change = color_list.index(previous_color)
-        color_befor_change - C = HR
-        if HR > 0 : 
-            HR * Move Head left()
-        if HR < 0 :
-             HR * -1 * MoveHead_Right()   
+    color_befor_change = 0 
+    for idx, color in color_list:
+        color_idx = idx
+        diff = color_befor_change - color_idx
+        diff = diff+1
+        if diff > 0 :
+            for x in range(diff):
+                Move Head left()
+        if diff < 0 :
+            diff = -diff
+            for x in range(diff):
+                MoveHead_Right()
+        color_befor_change = idx
+
     NeedleStorageArm_down()
     needleGear_engage()
-    msc#m1 low speed 3x rotations
-       
+    msc #TODO m1 low speed 3x rotations
 
-    if nps#needle in up position
+    if nps #needle in up position:
+        #TODO
+
+#Start embroidery
+def embroideryStart()
+    #TODO
