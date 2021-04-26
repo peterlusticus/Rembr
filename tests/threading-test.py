584abd270a1b2e10c7bc08d8b927d6e1279c2788
peterlusticus <71382635+peterlusticus@users.noreply.github.com>
2021-04-26 11:39:56 +0200

    Update threading-test.py

--- tests/threading-test.py
+++ tests/threading-test.py
@@ -1,12 +1,54 @@
 import time
+import threading
 
-def first():
-    for x in range(100):
-        print("1. Funktion")
+def embroideryStart():
+    for x in range(10):
+        time.sleep(.5)
+        print("[FUNCTION] embroideryStart(): "+x)
+    print("[FUNCTION] embroideryStart(): end")
 
-def second(): 
-    for x in range(100):
-        print("2. Funktion")
+def cncStart():
+    for x in range(5):
+        time.sleep(.5)
+        print("[FUNCTION] cncStart(): "+x)
+    print("[FUNCTION] cncStart(): end")
 
-first()
-second()
+def printColor():
+    embroidery = threading.Thread(target=embroideryStart)
+    cnc = threading.Thread(target=cncStart)
+    embroidery.start()
+    time.sleep(.1)
+    cnc.start()
+    embroidery.join()
+    cnc.join()
+    print("[FUNCTION] printColor(): end")
+    time.sleep(.1)
+
+def execute():
+    printColor()
+    print("[FUNCTION] execute(): end")
+    
+execute()
+GPIO.cleanup()
+exit()
+
+#Erwartete Konsolenausgabe:
+#[FUNCTION] embroideryStart(): 0
+#[FUNCTION] cncStart(): 0
+#[FUNCTION] embroideryStart(): 1
+#[FUNCTION] cncStart(): 1
+#[FUNCTION] embroideryStart(): 2
+#[FUNCTION] cncStart(): 2
+#[FUNCTION] embroideryStart(): 3
+#[FUNCTION] cncStart(): 3
+#[FUNCTION] embroideryStart(): 4
+#[FUNCTION] embroideryStart(): end
+#[FUNCTION] cncStart(): 4
+#[FUNCTION] cncStart(): 5
+#[FUNCTION] cncStart(): 6
+#[FUNCTION] cncStart(): 7
+#[FUNCTION] cncStart(): 8
+#[FUNCTION] cncStart(): 9
+#[FUNCTION] cncStart(): end
+#[FUNCTION] printColor(): end
+#[FUNCTION] execute(): end
